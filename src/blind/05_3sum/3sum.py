5ed23b0fe7ee88b810b1ac2c9d2129500311e266
ihabbou <10790679+ihabbou@users.noreply.github.com>
2022-05-01 20:10:36 +0200

    3sum O(nLog(n) + n)?

---
+++ src/blind/05_3sum/3sum.py
@@ -0,0 +1,42 @@
+# %%
+
+from typing import List, Set
+
+
+def threesum(nums: List[int]) -> List[List[int]]:
+
+    ppairs = dict()
+    sums = set()
+
+    for i, n in enumerate(nums):
+        for j, m in enumerate(nums):
+            if (i != j):
+                ppairs[n + m] = (i, j)
+
+    for i, n in enumerate(nums):
+        if -n in ppairs:
+            if i not in ppairs[-n]:
+                idx = ppairs[-n]
+                triplet = tuple(sorted([nums[idx[0]], nums[idx[1]], n]))
+                sums.add(triplet)
+
+    return [list(t) for t in sums]
+
+# %% test
+
+
+inputs = [
+    [-1, 0, 1, 2, -1, -4],
+    [],
+    [0],
+]
+
+outputs = [
+    [[-1, -1, 2], [-1, 0, 1]],
+    [],
+    [],
+]
+
+for nums, expected in zip(inputs, outputs):
+    result = threesum(nums)
+    assert result == expected, f"\nExpected {expected}, \ngot\t {result}"
